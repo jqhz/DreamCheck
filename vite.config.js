846ab89d7a459bdc787e8784ea750c844a6ef38c
jqhz <jasonhuang3030@gmail.com>
2025-07-16 16:05:00 -0500

    fix gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DreamCheck', // <-- Set this to your repo name, with slashes
+  base: '/DreamCheck/', // <-- Set this to your repo name, with slashes
 })
